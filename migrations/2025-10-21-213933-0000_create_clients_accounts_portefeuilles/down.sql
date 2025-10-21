03e07bad2ef0380f4f50f13c17b848e4daa1ee4e
Nadaya <nada.yassine042@gmail.com>
2025-10-21 18:16:19 -0400

    migration reproductibles

---
+++ migrations/2025-10-21-213933-0000_create_clients_accounts_portefeuilles/down.sql
@@ -0,0 +1,6 @@
+-- This file should undo anything in `up.sql`
+
+DROP TABLE IF EXISTS clients CASCADE;
+DROP TABLE IF EXISTS portfeuille CASCADE;
+DROP TABLE IF EXISTS account CASCADE;
+DROP TABLE IF EXISTS transactions CASCADE;
---
+++ migrations/2025-10-21-213933-0000_create_clients_accounts_portefeuilles/up.sql
@@ -0,0 +1,43 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS public.clients
+(
+    client_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default",
+    email character varying(255) COLLATE pg_catalog."default",
+    phone character varying(20) COLLATE pg_catalog."default",
+
+    CONSTRAINT clients_pkey PRIMARY KEY (client_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.portefeuille
+(
+    portefeuille_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    balance integer NOT NULL,
+
+    CONSTRAINT portefeuille_pkey PRIMARY KEY (portefeuille_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.account
+(
+    account_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    username character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    client_id INT NOT NULL REFERENCES clients(client_id) ON DELETE CASCADE,
+    portefeuille_id INT NOT NULL REFERENCES portefeuille(portefeuille_id) ON DELETE CASCADE,
+    status VARCHAR(20) NOT NULL DEFAULT 'Pending', 
+    mfa_enabled BOOLEAN NOT NULL DEFAULT false,
+    
+    CONSTRAINT accounts_pkey PRIMARY KEY (account_id),
+    CONSTRAINT accounts_username_key UNIQUE (username)
+);
+
+CREATE TABLE IF NOT EXISTS public.transactions
+(
+    transaction_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    portefeuille_id integer NOT NULL,
+    amount integer NOT NULL,
+    transaction_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT transactions_pkey PRIMARY KEY (transaction_id),
+    CONSTRAINT fk_portefeuille FOREIGN KEY (portefeuille_id) REFERENCES public.portefeuille (portefeuille_id) ON DELETE CASCADE
+);
